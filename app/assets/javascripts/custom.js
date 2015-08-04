88d523c5e46dfcb393f962a2fada1e8fb8dca0b4
HoangVanTrinh92 <hoang.van.trinh@framgia.com>
2015-08-04 13:09:58 +0700

    fix-upload-image-error

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -57,101 +57,44 @@ $(document).ready(function(){
   var tabAddImage = $("#tab-add-image");
   var tabContent = $(".tab-content");
 
-  if (uploadImage.length > 0) {
-    $(".btn-action-show-images").attr("class", "btn-action-show-images btn btn-info toggler active");
-  }
+  if(uploadImage != null) {
+    if (uploadImage.length > 0) {
+      $(".btn-action-show-images").attr("class", "btn-action-show-images btn btn-info toggler active");
+    }
 
-  for (var i = 0; i < uploadImage.length; i++) {
-    var new_id = new Date().getTime();
-    var link = uploadImage[i].photo.url;
+    for (var i = 0; i < uploadImage.length; i++) {
+      var new_id = new Date().getTime();
+      var link = uploadImage[i].photo.url;
 
-    var checkbox = uploadImage[i].is_main == true ? "checked" : "";
-    tabAddImage.find("li.active").attr("class", "");
+      var checkbox = uploadImage[i].is_main == true ? "checked" : "";
+      tabAddImage.find("li.active").attr("class", "");
 
-    var new_tab = $("<li class=\"active\"><a data-toggle=\"tab\" href=" + "#" + new_id + ">New Image</a></li>");
-    tabAddImage.append(new_tab);
+      var new_tab = $("<li class=\"active\"><a data-toggle=\"tab\" href=" + "#" + new_id + ">New Image</a></li>");
+      tabAddImage.append(new_tab);
 
-    tabContent.find("div.active").attr("class", "tab-pane fade");
+      tabContent.find("div.active").attr("class", "tab-pane fade");
 
-    var divTabPane = $("<div id=" + new_id + " class= \"tab-pane active\"><input type=\"file\" id=" + "address_images_attributes_" + new_id + "_photo" + " name=" + "address[images_attributes][" + new_id + "][photo]" + " data-fileupload=\"true\" class=\"file-address\"><img alt=" + new_id + " src=" + link + " class=\"img-preview img-thumbnail preview\"/><div id=\"tab-pane-right\"><input id=" + "address_images_attributes_" + new_id + "__destroy" + " type=\"hidden\" name=" + "address[images_attributes][" + new_id + "][_destroy]" + " value=\"false\"><input id=" + "address_images_attributes_" + new_id + "_id" + " type=\"hidden\" name=" + "address[images_attributes][" + new_id + "][id]" + " value=" + uploadImage[i].id + "><a href=\"javascript:void(0);\" rel=\"tooltip\" title=\"Remove this image\" class=\"glyphicon glyphicon-trash\" onclick=\"remove_fields(this)\" data-association=\"images\"></a></div><div class=\"set-main-checkbox\"><input type=\"hidden\" value=\"0\" name=" + "address[images_attributes][" + new_id + "][is_main]" + "><input id=" + "address_images_attributes_" + new_id + "_is_main" + " type=\"checkbox\" name=" + "address[images_attributes][" + new_id + "][is_main]" + " " + checkbox + " class=\"is-main-checkbox\" value=\"1\"> Choose this image is main image</div></div>");
-    tabContent.append(divTabPane);
+      var divTabPane = $("<div id=" + new_id + " class= \"tab-pane active\"><input type=\"file\" id=" + "address_images_attributes_" + new_id + "_photo" + " name=" + "address[images_attributes][" + new_id + "][photo]" + " data-fileupload=\"true\" class=\"file-address\"><img alt=" + new_id + " src=" + link + " class=\"img-preview img-thumbnail preview\"/><div id=\"tab-pane-right\"><input id=" + "address_images_attributes_" + new_id + "__destroy" + " type=\"hidden\" name=" + "address[images_attributes][" + new_id + "][_destroy]" + " value=\"false\"><input id=" + "address_images_attributes_" + new_id + "_id" + " type=\"hidden\" name=" + "address[images_attributes][" + new_id + "][id]" + " value=" + uploadImage[i].id + "><a href=\"javascript:void(0);\" rel=\"tooltip\" title=\"Remove this image\" class=\"glyphicon glyphicon-trash\" onclick=\"remove_fields(this)\" data-association=\"images\"></a></div><div class=\"set-main-checkbox\"><input type=\"hidden\" value=\"0\" name=" + "address[images_attributes][" + new_id + "][is_main]" + "><input id=" + "address_images_attributes_" + new_id + "_is_main" + " type=\"checkbox\" name=" + "address[images_attributes][" + new_id + "][is_main]" + " " + checkbox + " class=\"is-main-checkbox\" value=\"1\"> Choose this image is main image</div></div>");
+      tabContent.append(divTabPane);
 
-    var activeTab = $("#" + new_id);
-    var fname = activeTab.find("img").attr("src").substr(25, 31);
-    if (fname === "" || fname === null )
-      $("a[href=#" + new_id + "]").html("New Image");
-    else
-      $("a[href=#" + new_id + "]").html(fname);
-
-    $("#" + new_id).change(function() {
-      var filename = $(this).find("img").attr("src").substr(25, 31);
-      var lastIndex = filename.lastIndexOf("\\");
-      if (lastIndex >= 0) {
-        filename = filename.substring(lastIndex + 1);
-      }
-      if (filename === "" || filename === null )
-        $("a[href=#" + $(this).attr("id") + "]").html("New Image");
+      var activeTab = $("#" + new_id);
+      var fname = activeTab.find("img").attr("src").substr(25, 31);
+      if (fname === "" || fname === null )
+        $("a[href=#" + new_id + "]").html("New Image");
       else
-        $("a[href=#" + $(this).attr("id") + "]").html(filename);
-    });
+        $("a[href=#" + new_id + "]").html(fname);
+
+      $("#" + new_id).change(function() {
+        var filename = $(this).find("img").attr("src").substr(25, 31);
+        var lastIndex = filename.lastIndexOf("\\");
+        if (lastIndex >= 0) {
+          filename = filename.substring(lastIndex + 1);
+        }
+        if (filename === "" || filename === null )
+          $("a[href=#" + $(this).attr("id") + "]").html("New Image");
+        else
+          $("a[href=#" + $(this).attr("id") + "]").html(filename);
+      });
+    }
   }
 });
-
-function remove_fields(link) {
-  var tabAddImage = $("#tab-add-image");
-  li_current_active = tabAddImage.find("li.active");
-  li_previous = li_current_active.prev();
-
-  li_previous.attr("class", "active");
-  li_current_active.hide();
-
-  var tabContent = $(".tab-content");
-  div_curr_active = tabContent.find("div.active");
-  div_curr_active.find("[id$=_destroy]").attr("value", "1");
-
-  div_pre_active = div_curr_active.prev();
-  div_pre_active.attr("class", "tab-pane active")
-  div_curr_active.hide();
-
-  if (tabContent.find("div").length === 0) {
-    $("#images-form").hide("slow");
-    $(".btn-action-show-images").attr("class", "btn-action-show-images btn btn-info toggler disabled");
-    $(".btn-action-show-images").find("i").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-right");
-  }
-}
-
-function add_fields(link, association, content) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g");
-
-  var tabAddImage = $("#tab-add-image");
-  tabAddImage.find("li.active").attr("class", "");
-
-  var new_tab = ("<li class=\"active\"><a data-toggle=\"tab\" href=" + "#" + new_id + ">New Image</a></li>");
-  tabAddImage.append(new_tab);
-
-  var tabContent = $(".tab-content");
-  tabContent.find("div.active").attr("class", "tab-pane fade");
-
-  var divTabPane = ("<div id=" + new_id + " class= \"tab-pane active\">" + content.replace(regexp, new_id) + " <input id=" + "address_images_attributes_" + new_id + "_destroy" + " name=" + "address[images_attributes][" + new_id + "][_destroy]" + " type=\"hidden\" value=\"false\"></div>");
-  tabContent.append(divTabPane);
-
-  $(".btn-action-show-images").attr("class", "btn-action-show-images btn btn-info toggler active");
-  $(".btn-action-show-images").find("i").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");
-
-  $("#images-form").show("slow");
-
-  $("#" + new_id).change(function() {
-    var filename = $(this).find("input[type=file]").val();
-    var lastIndex = filename.lastIndexOf("\\");
-    if (lastIndex >= 0) {
-      filename = filename.substring(lastIndex + 1);
-    }
-    if (filename === "" || filename === null )
-      $("a[href=#" + $(this).attr("id") + "]").html("New Image");
-    else
-      $("a[href=#" + $(this).attr("id") + "]").html(filename);
-  });
-}
-
-$(".tooltip").tooltip();
