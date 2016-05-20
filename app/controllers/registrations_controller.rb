5e9c7048cee60024fc33d8fd9145556f2c4ed39c
Dao Thanh Cam <daothanhcam@gmail.com>
2016-05-20 18:04:04 +0700

    edit update user without password required

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,4 +3,8 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit User::PARAMS_ATTRIBUTES
   end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 end
